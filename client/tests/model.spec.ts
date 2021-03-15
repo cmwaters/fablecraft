fadc52fb0f211f025db3d10eacedab7a05946247
Callum Waters <cmwaters19@gmail.com>
2021-03-15 11:11:40 +0100

    begin to add model tests

---
+++ client/tests/model.spec.ts
@@ -0,0 +1,13 @@
+import { expect } from "chai"
+
+
+describe("Model | LocalStorage", () => {
+
+    it("can save a new story", async done => {
+
+        expect(1 + 1).to.equal(2)
+        done()
+
+    })
+
+})
